ostroproject/ostro-os

gcc-cross-canadian: Fix include paths and hence c++ compiler issues
(From OE-core rev: d6c7a44b94aa0f871d3ad1a2097a4a8d773eabf5)

Signed-off-by: Richard Purdie <rpurdie@linux.intel.com>

meta/packages/gcc/gcc-cross-canadian_4.3.3.bb
@@ -5,7 +5,7 @@ require gcc-cross-canadian.inc
 require gcc-configure-sdk.inc
 require gcc-package-sdk.inc
 
-PR = "r8"
+PR = "r9"
 
 DEPENDS += "gmp-nativesdk mpfr-nativesdk"
 RDEPENDS_${PN} += "mpfr-nativesdk"

yoctoproject/poky

meta/packages/gcc/gcc-cross-canadian_4.3.3.bb
@@ -5,7 +5,7 @@ require gcc-cross-canadian.inc
 require gcc-configure-sdk.inc
 require gcc-package-sdk.inc
 
-PR = "r7"
+PR = "r8"
 
 DEPENDS += "gmp-nativesdk mpfr-nativesdk"
 RDEPENDS_${PN} += "mpfr-nativesdk"