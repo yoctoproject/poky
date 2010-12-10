ostroproject/ostro-os

busybox: import upstream patch for make 3.82
(From OE-core rev: 4f6d91ec214d9d42ae9477f64dbaceb9477e4d0d)

Signed-off-by: Joshua Lock <josh@linux.intel.com>

meta/recipes-core/busybox/busybox_1.16.2.bb
@@ -1,5 +1,5 @@
 require busybox.inc
-PR = "r2"
+PR = "r3"
 
 SRC_URI = "http://www.busybox.net/downloads/busybox-${PV}.tar.bz2;name=tarball \
            file://udhcpscript.patch \
@@ -8,6 +8,7 @@ SRC_URI = "http://www.busybox.net/downloads/busybox-${PV}.tar.bz2;name=tarball \
            file://get_header_tar.patch \
            file://busybox-appletlib-dependency.patch \
            file://run-parts.in.usr-bin.patch \
+           file://make-382-fix.patch \
            file://find-touchscreen.sh \
            file://busybox-cron \
            file://busybox-httpd \

yoctoproject/poky

meta/recipes-core/busybox/busybox_1.16.2.bb
@@ -1,5 +1,5 @@
 require busybox.inc
-PR = "r0"
+PR = "r1"
 
 SRC_URI = "http://www.busybox.net/downloads/busybox-${PV}.tar.bz2;name=tarball \
            file://udhcpscript.patch \
@@ -8,6 +8,7 @@ SRC_URI = "http://www.busybox.net/downloads/busybox-${PV}.tar.bz2;name=tarball \
            file://get_header_tar.patch \
            file://busybox-appletlib-dependency.patch \
            file://run-parts.in.usr-bin.patch \
+           file://make-382-fix.patch \
            file://find-touchscreen.sh \
            file://busybox-cron \
            file://busybox-httpd \