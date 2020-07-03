54d38a91e109cda79e5b19bc2965f8a73a468fbc
Nicolas Dechesne <nicolas.dechesne@linaro.org>
2020-07-03 17:56:43 +0200

    sphinx: add boilerplate file
    A certain amount of boilerplate is added at the beginning of all
    documents. In DocBook this is copy/pasted in each file. Let's create a
    boilerplate ReST file, which we will include in each document,
    wherever it's required.
    
    (From yocto-docs rev: 37e0d5f246c614e62a7c0d4d72a5d6ce9ec5325e)
    
    Signed-off-by: Nicolas Dechesne <nicolas.dechesne@linaro.org>
    Signed-off-by: Richard Purdie <richard.purdie@linuxfoundation.org>

--- documentation/conf.py
+++ documentation/conf.py
@@ -43,7 +43,7 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'boilerplate.rst']
 
 # master document name. The default changed from contents to index. so better
 # set it ourselves.
