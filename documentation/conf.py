0ce3f64fce7b6a9075abd306a7ef228d48a42035
Nicolas Dechesne <nicolas.dechesne@linaro.org>
2020-09-15 17:13:18 +0200

    sphinx: conf: exclude adt-manual/*.rst
    The ADT Manual is deprecated, and was removed from the documentation
    set in 2.2, until we remove it completely, let's make sure it's
    excluded from Sphinx build.
    
    (From yocto-docs rev: 5fa20d6afb1be56cbb2a012357f8ccff4b91d585)
    
    Signed-off-by: Nicolas Dechesne <nicolas.dechesne@linaro.org>
    Signed-off-by: Richard Purdie <richard.purdie@linuxfoundation.org>

--- documentation/conf.py
+++ documentation/conf.py
@@ -53,7 +53,8 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'boilerplate.rst']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'boilerplate.rst',
+                    'adt-manual/*.rst']
 
 # master document name. The default changed from contents to index. so better
 # set it ourselves.
