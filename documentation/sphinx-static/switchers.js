d09e2e05a0391ad4d718214516d92a160f89767e
Michael Halstead <mhalstead@linuxfoundation.org>
2022-02-16 23:04:15 -0800

    releases: update to include 3.1.14
    Adding 3.1.14 to documentation switcher and release list.
    
    (From yocto-docs rev: 4b2cfbc517208d6df40104d25e62f8febdd76a7f)
    
    Signed-off-by: Michael Halstead <mhalstead@linuxfoundation.org>
    Signed-off-by: Richard Purdie <richard.purdie@linuxfoundation.org>

--- documentation/sphinx-static/switchers.js
+++ documentation/sphinx-static/switchers.js
@@ -14,7 +14,7 @@ by https://git.yoctoproject.org/yocto-autobuilder-helper/tree/scripts/run-docs-b
     '3.4.1': '3.4.1',
     '3.3.4': '3.3.4',
     '3.2.4': '3.2.4',
-    '3.1.13': '3.1.13',
+    '3.1.14': '3.1.14',
     '3.0.4': '3.0.4',
     '2.7.4': '2.7.4',
   };
