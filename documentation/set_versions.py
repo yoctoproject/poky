7acf97ad3ba06a5eca777fb667bcd15c567781d1
Quentin Schulz <quentin.schulz@theobroma-systems.com>
2022-04-28 10:17:06 +0200

    docs: set_versions.py: remove hardknott from active releases list
    Hardknott 3.3.6 is the last release of the series and the release is now
    obsolete.
    
    Let's remove hardknott from the list of active releases.
    
    Cc: Quentin Schulz <foss+yocto@0leil.net>
    (From yocto-docs rev: 4ecce89b6a5d5feb0d31c735a39619e73eeb5e44)
    
    Signed-off-by: Quentin Schulz <quentin.schulz@theobroma-systems.com>
    Reviewed-by: Michael Opdenacker <michael.opdenacker@bootlin.com>
    Signed-off-by: Richard Purdie <richard.purdie@linuxfoundation.org>

--- documentation/set_versions.py
+++ documentation/set_versions.py
@@ -23,7 +23,7 @@ ourversion = None
 if len(sys.argv) == 2:
     ourversion = sys.argv[1]
 
-activereleases = ["kirkstone", "honister", "hardknott", "dunfell"]
+activereleases = ["kirkstone", "honister", "dunfell"]
 devbranch = "langdale"
 ltsseries = ["kirkstone", "dunfell"]
 
