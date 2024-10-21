ef5664a36e28145b2aaf57f2052366b7ad623a25
Richard Purdie <richard.purdie@linuxfoundation.org>
2024-10-21 12:45:43 +0100

    bitbake: try own until function
    (Bitbake rev: 1b011be5bedb679fca6d0a7775ea9dd6a8fdca47)
    
    Signed-off-by: Richard Purdie <richard.purdie@linuxfoundation.org>

--- bitbake/lib/toaster/tests/browser/selenium_helpers_base.py
+++ bitbake/lib/toaster/tests/browser/selenium_helpers_base.py
@@ -236,17 +236,9 @@ class SeleniumTestCaseBase(unittest.TestCase):
     def wait_until_clickable(self, selector, timeout=Wait._TIMEOUT):
         """ Wait until element matching CSS selector is visible on the page """
         WebDriverWait(self.driver, timeout=timeout).until(lambda driver: self.driver.execute_script("return jQuery.active == 0"))
-        sel = selector
-        if sel.startswith('#'):
-            sel = selector[1:]
-        WebDriverWait(
-            self.driver,
-            timeout=timeout,
-        ).until(
-            EC.element_to_be_clickable((By.ID, sel
-                                        )
-                                       )
-        )
+        is_clickable = lambda driver: (self.find(selector).is_displayed() and self.find(selector).is_enabled())
+        msg = 'An element matching "%s" should be clickable' % selector
+        Wait(self.driver, timeout=timeout).until(is_clickable, msg)
         time.sleep(1)
         return self.find(selector)
 
